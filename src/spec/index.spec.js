ec0c6df02d38f04f1a7f0f4c2fdf50db8f11478b
blmage <bl.mage.fr@gmail.com>
2020-07-19 20:22:31 +0200

    Remove the obsolete tests

--- src/spec/index.spec.js
+++ src/spec/index.spec.js
@@ -26,31 +26,6 @@ describe('compareWords', () => {
         second: 'sealed',
         expected: 0.8,
       }, {
-        first: 'web applications',
-        second: 'applications of the web',
-        expected: 0.7878787878787878,
-      }, {
-        first: 'this will have a typo somewhere',
-        second: 'this will huve a typo somewhere',
-        expected: 0.92,
-      }, {
-        first: 'Olive-green table for sale, in extremely good condition.',
-        second: 'For sale: table in very good  condition, olive green in colour.',
-        expected: 0.6060606060606061,
-      },
-      {
-        first: 'Olive-green table for sale, in extremely good condition.',
-        second: 'For sale: green Subaru Impreza, 210,000 miles',
-        expected: 0.2558139534883721,
-      }, {
-        first: 'Olive-green table for sale, in extremely good condition.',
-        second: 'Wanted: mountain bike with at least 21 gears.',
-        expected: 0.1411764705882353,
-      }, {
-        first: 'this has one extra word',
-        second: 'this has one word',
-        expected: 0.7741935483870968,
-      }, {
         first: 'a',
         second: 'a',
         expected: 1,
@@ -70,15 +45,7 @@ describe('compareWords', () => {
         first: '',
         second: 'a',
         expected: 0,
-      }, {
-        first: 'apple event',
-        second: 'apple    event',
-        expected: 1,
-      }, {
-        first: 'iphone',
-        second: 'iphone x',
-        expected: 0.9090909090909091,
-      }
+      },
     ];
 
     testData.forEach(test => {
