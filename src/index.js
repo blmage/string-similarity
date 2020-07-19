d9afa0449115b64b1eb5297757ad78e66450a1db
blmage <bl.mage.fr@gmail.com>
2020-07-19 20:13:59 +0200

    Clean up the code

--- src/index.js
+++ src/index.js
@@ -1,39 +1,57 @@
 module.exports = {
-	compareTwoStrings,
+  compareTwoStrings,
 };
 
 function compareTwoStrings(first, second) {
-	first = first.replace(/\s+/g, '')
-	second = second.replace(/\s+/g, '')
+  first = first.replace(/\s+/g, '')
+  second = second.replace(/\s+/g, '')
 
-	if (!first.length && !second.length) return 1;                   // if both are empty strings
-	if (!first.length || !second.length) return 0;                   // if only one is empty string
-	if (first === second) return 1;       							 // identical
-	if (first.length === 1 && second.length === 1) return 0;         // both are 1-letter strings
-	if (first.length < 2 || second.length < 2) return 0;			 // if either is a 1-letter string
+  if (!first.length && !second.length) {
+    return 1;
+  }
 
-	let firstBigrams = new Map();
-	for (let i = 0; i < first.length - 1; i++) {
-		const bigram = first.substring(i, i + 2);
-		const count = firstBigrams.has(bigram)
-			? firstBigrams.get(bigram) + 1
-			: 1;
+  if (!first.length || !second.length) {
+    return 0;
+  }
 
-		firstBigrams.set(bigram, count);
-	};
+  if (first === second) {
+    return 1;
+  }
 
-	let intersectionSize = 0;
-	for (let i = 0; i < second.length - 1; i++) {
-		const bigram = second.substring(i, i + 2);
-		const count = firstBigrams.has(bigram)
-			? firstBigrams.get(bigram)
-			: 0;
+  if ((first.length === 1) && (second.length === 1)) {
+    return 0;
+  }
 
-		if (count > 0) {
-			firstBigrams.set(bigram, count - 1);
-			intersectionSize++;
-		}
-	}
+  if ((first.length < 2) || (second.length < 2)) {
+    return 0;
+  }
 
-	return (2.0 * intersectionSize) / (first.length + second.length - 2);
+  let firstBigrams = new Map();
+
+  for (let i = 0; i < first.length - 1; i++) {
+    const bigram = first.substring(i, i + 2);
+
+    const count = firstBigrams.has(bigram)
+      ? firstBigrams.get(bigram) + 1
+      : 1;
+
+    firstBigrams.set(bigram, count);
+  }
+
+  let intersectionSize = 0;
+
+  for (let i = 0; i < second.length - 1; i++) {
+    const bigram = second.substring(i, i + 2);
+
+    const count = firstBigrams.has(bigram)
+      ? firstBigrams.get(bigram)
+      : 0;
+
+    if (count > 0) {
+      firstBigrams.set(bigram, count - 1);
+      intersectionSize++;
+    }
+  }
+
+  return (2.0 * intersectionSize) / (first.length + second.length - 2);
 }
